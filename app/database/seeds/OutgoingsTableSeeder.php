881a047323c54b8c3620521db9359d81bda4ea01
Alessandro <alessandro.rovito@gmail.com>
2013-10-01 13:48:22 +0200

    Update OutgoingsTableSeeder.php

--- app/database/seeds/OutgoingsTableSeeder.php
+++ app/database/seeds/OutgoingsTableSeeder.php
@@ -1,19 +1,33 @@
 <?php
-
+ 
 class OutgoingsTableSeeder extends Seeder {
-
-	public function run()
-	{
-		// Uncomment the below to wipe the table clean before populating
-		DB::table('outgoings')->truncate();
-
-		Outgoings::create(array(
-			'user_id' => 1,
-			'title' => 'spesa',
-			'description' => 'descrizione',
-			'amount' => '18,23'
-		));
-
-	}
-
+ 
+    public function run()
+    {
+        DB::table('outgoings')->delete();
+ 
+        Outgoings::create(array(
+            'user_id' => '1',
+            'title' => 'Spesa 1',
+            'description' => 'Descrizione',
+            'date' => '2013-09-27',
+            'amount' => '25,45'
+        ));
+ 
+        Outgoings::create(array(
+            'user_id' => '1',
+            'title' => 'Spesa 2',
+            'description' => 'Descrizione',
+            'date' => '2013-09-27',
+            'amount' => '25,45'
+        ));
+        Outgoings::create(array(
+            'user_id' => '1',
+            'title' => 'Spesa 3',
+            'description' => 'Descrizione',
+            'date' => '2013-09-27',
+            'amount' => '25,45'
+        ));
+    }
+ 
 }
